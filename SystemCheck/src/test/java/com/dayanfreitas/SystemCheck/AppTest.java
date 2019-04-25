a50398636c7c92325a0a947863af939fd45ea05f
dayanFreitas <dayan.freitas@catolicasc.org.br>
2019-04-25 20:17:31 -0300

    Importação não usadas deletadas

--- SystemCheck/src/test/java/com/dayanfreitas/SystemCheck/AppTest.java
+++ SystemCheck/src/test/java/com/dayanfreitas/SystemCheck/AppTest.java
@@ -1,12 +1,7 @@
 package com.dayanfreitas.SystemCheck;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest extends TestCase {
 	// Falta implementar
 	public void testOi() {
